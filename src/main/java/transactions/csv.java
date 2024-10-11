32185e6572c3f4f14b105eec5b09175275253e14
Mehak <mehakmatharu@Mehaks-MacBook-Air.local>
2024-10-11 11:57:35 -0700

    First Capstone Project

---
+++ src/main/java/transactions/csv.java
@@ -0,0 +1,12 @@
+package transactions;
+
+public class csv {
+    public static void main(String[] args) {
+        /* Each transaction will have the following format
+        date|time|description|vendor|amount
+        2023-04-15|10:13:25|ergonomic keyboard|Amazon|-89.50
+        2023-04-15|11:15:00|Invoice 1001 paid|Joe|1500.00
+        */
+    }
+}
+
